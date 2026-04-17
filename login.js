5bcd8ca9475884a8570e1524ecb16f9b767dc596
Mateus-alt <145471906+Mateus-alt@users.noreply.github.com>
2026-04-17 13:29:23 -0300

    Update login.js
    Redirecionando para tela de criação de eventos

--- login.js
+++ login.js
@@ -52,5 +52,5 @@ botaoLogin.addEventListener('click', function (event) {
     }
 
     mostrarMensagem('Login bem-sucedido');
-    // Aqui você pode adicionar a lógica de redirecionamento para a área administrativa.
+    window.location.href = "admin_eventos.html";
 });
